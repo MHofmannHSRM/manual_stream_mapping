a09308d55a2ada0bf2f7c6921c12d7759705a774
MHofmannHSRM <matthias.hofmann@student.hs-rm.de>
2024-04-25 17:05:07 +0200

    FIX: Changed column type to correctly display multiple objects in sender/receiver table view for format type

--- netbox_multicast_stream_mapping/tables.py
+++ netbox_multicast_stream_mapping/tables.py
@@ -1,16 +1,34 @@
 import django_tables2 as tables
 
-from netbox.tables import NetBoxTable, ChoiceFieldColumn, TagColumn
+from netbox.tables import NetBoxTable, ChoiceFieldColumn, TagColumn, ManyToManyColumn
 
 from .models import Processor, Sender, Receiver, Stream, Format
+
+
+class FormatTable(NetBoxTable):
+    name = tables.Column(linkify=True)
+    type = ChoiceFieldColumn()
+    res_h = tables.Column(verbose_name='Vertical Resolution')
+    res_w = tables.Column(verbose_name='Horizontal Resolution')
+    fps = ChoiceFieldColumn(verbose_name='Frame Rate')
+    audio_ch = tables.Column(verbose_name='Number of Audio Channels')
+    comments = tables.Column()
+    description = tables.Column()
+    tags = TagColumn()  # TODO -> Verlinkung -> Filter?
+
+    class Meta(NetBoxTable.Meta):
+        model = Format
+        # template_name = 'utilities/tables/netbox_table.html' TODO
+        fields = ('pk', 'id', 'name', 'type', 'res_h', 'res_w', 'fps', 'audio_ch', 'comments', 'description', 'tags') # todo updated?
+        default_columns = ('name', 'type', 'res_h', 'res_w', 'fps', 'audio_ch', 'description', 'tags')
 
 
 class ProcessorTable(NetBoxTable):
     name = tables.Column(linkify=True)
     device = tables.Column(linkify=True)
     module = tables.Column(linkify=True)
-    sender_count = tables.Column() # todo counter cache field
-    receiver_count = tables.Column() # todo counter cache field
+    sender_count = tables.Column(verbose_name='Number of Senders') # todo counter cache field
+    receiver_count = tables.Column(verbose_name='Number of Receivers') # todo counter cache field
     description = tables.Column()
     comments = tables.Column()
     tags = TagColumn() # TODO -> Verlinkung -> Filter?
@@ -27,11 +45,11 @@ class ProcessorTable(NetBoxTable):
 class SenderTable(NetBoxTable):
     name = tables.Column(linkify=True)
     processor = tables.Column(linkify=True)
-    sender_ip = tables.Column(linkify=True, verbose_name='Receiver IP-Adress')
+    sender_ip = tables.Column(linkify=True, verbose_name='Sender IP-Adress')
     max_bandwidth_out = tables.Column(verbose_name='Max. Bandwidth (Mbps)')
-    supported_formats = tables.Column()
-    switch_method = tables.Column()
-    signal_type = tables.Column()
+    supported_formats = ManyToManyColumn()
+    switch_method = tables.Column(verbose_name='Switch Method (2022-7)')
+    signal_type = ChoiceFieldColumn(verbose_name='Signal Type')
     description = tables.Column()
     comments = tables.Column()
     tags = TagColumn()  # TODO -> Verlinkung -> Filter?
@@ -49,9 +67,9 @@ class ReceiverTable(NetBoxTable):
     processor = tables.Column(linkify=True)
     receiver_ip = tables.Column(linkify=True, verbose_name='Receiver IP-Adress')
     max_bandwidth_in = tables.Column(verbose_name='Max. Bandwidth (Mbps)')
-    supported_formats = tables.Column()
-    switch_method = tables.Column()
-    signal_type = tables.Column()
+    supported_formats = ManyToManyColumn()
+    switch_method = ChoiceFieldColumn(verbose_name='Switch Method (2022-7)')
+    signal_type = ChoiceFieldColumn(verbose_name='Signal Type')
     comments = tables.Column()
     description = tables.Column()
     tags = TagColumn()  # TODO -> Verlinkung -> Filter?
@@ -68,7 +86,7 @@ class StreamTable(NetBoxTable):
     name = tables.Column(linkify=True)
     processor = tables.Column()
     sender = tables.Column()
-    receivers = tables.Column()
+    receivers = tables.Column() # todo ändern
     bandwidth = tables.Column() #rechtschreibfehler überall
     format = tables.Column()
     signal_type = tables.Column()
@@ -84,22 +102,4 @@ class StreamTable(NetBoxTable):
         fields = ('pk', 'id', 'name', 'processor', 'sender', 'receivers', 'bandwidth', 'format', 'signal_type',
                   'protocol', 'audio_channels', 'comments', 'description', 'tags') # todo updated?
         default_columns = ('name', 'signal_type', 'sender', 'receivers', 'description', 'tags')
-
-
-class FormatTable(NetBoxTable):
-    name = tables.Column(linkify=True)
-    type = tables.Column()
-    res_h = tables.Column()
-    res_w = tables.Column()
-    fps = tables.Column()
-    audio_ch = tables.Column()
-    comments = tables.Column()
-    description = tables.Column()
-    tags = TagColumn()  # TODO -> Verlinkung -> Filter?
-
-    class Meta(NetBoxTable.Meta):
-        model = Format
-        # template_name = 'utilities/tables/netbox_table.html' TODO
-        fields = ('pk', 'id', 'name', 'type', 'res_h', 'res_w', 'fps', 'audio_ch', 'comments', 'description', 'tags') # todo updated?
-        default_columns = ('name', 'type', 'res_h', 'res_w', 'fps', 'audio_ch', 'description', 'tags')
 
